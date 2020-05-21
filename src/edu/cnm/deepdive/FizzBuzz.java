b1fc8fff4ad505335c0f21433b5c9499117fb37a
levisanchez <levijames@gmail.com>
2020-05-21 14:27:20 -0600

    Keep fizzbuzz status in string

--- src/edu/cnm/deepdive/FizzBuzz.java
+++ src/edu/cnm/deepdive/FizzBuzz.java
@@ -5,24 +5,19 @@ public class FizzBuzz {
   public static void main(String[] args) {
 
     for (int counter = 0; counter <= 100; counter++) {
-      //  - Check whether the current counter value is fizz, buzz, or both.
+      String status = "";
       boolean counterIsFizz = (counter % 3 == 0);
       boolean counterIsBuzz = (counter % 5 == 0);
-
-      // TODO Print the number, or fizz, or buzz, or fizzbuzz, as appropriate
-      // If counterIsFizz and counterIsBuzz, print fizzbuzz;
-      // otherwise, if counterIsFizz, then print fizz;
-      // otherwise, if counterIsBuzz, then print buzz;
-      if (counterIsFizz && counterIsBuzz) {
-        System.out.println("fizzbuzz");
-
-      } else if (counterIsFizz) {
-        System.out.println("fizz");
-      } else if (counterIsBuzz) {
-        System.out.println("buzz");
-      } else {
-        System.out.println(counter);
+      if (counterIsFizz) {
+        status += "fizz";
       }
+      if (counterIsBuzz) {
+        status += "buzz";
+      }
+      if (status.isEmpty()){
+        status += counter;
+      }
+      System.out.println(status);
 
     }
   }
